10a1d851082c6cb6b5df8d1c6851457f244162fe
WilliamMyers <willy@wdmyers.co.uk>
2020-04-02 15:37:07 +0100

    next exercise

--- fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_12/FunctionExamples.java
+++ fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_12/FunctionExamples.java
@@ -6,17 +6,20 @@ public class FunctionExamples {
    * Note: The same implementation may be used for all four cases.
    */
 
-  public static final Function<Integer, Integer> factorial0 = null; // To be implemented
+  //static reference to the function...FunctionExamples.xxxx
+  public static final Function<Integer, Integer> factorial0 = n -> n <= 1 ? n : n * FunctionExamples.factorial0.apply(n - 1);
 
   public static Function<Integer, Integer> factorial1;
   static {
-    factorial1 = null; // To be implemented
+    factorial1 = n -> n <= 1 ? n : n * FunctionExamples.factorial1.apply(n - 1);
   }
 
-  public final Function<Integer, Integer> factorial2 = null; // To be implemented
+  //final requires this.xxx
+  public final Function<Integer, Integer> factorial2 = n -> n <= 1 ? n : n * this.factorial2.apply(n - 1);
 
+  //declaring in a method works because the method is declared before it is used
   public Function<Integer, Integer> factorial3;
   {
-    factorial3 = null; // To be implemented
+    factorial3 = n -> n <= 1 ? n : n * factorial3.apply(n - 1);
   }
 }
